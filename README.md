75efe1e0d0b9f2a57f062de9016f8005f1fb1868
Saabstory88 <pete@clearstorycreative.com>
2017-05-31 10:46:19 -0400

    Begin file Strcture

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # VideoServer
+
+This was built for a specific project.
+
+No support provided if anyone choses to use this application
--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const hbs = require('hbs');
+let video = require('./routes/video');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+//Register view engine components
+hbs.registerPartials(__dirname + '/views/parts');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,12 +24,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', video);
+app.use('/video', video);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
